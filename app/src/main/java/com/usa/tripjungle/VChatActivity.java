bcc080da151a6f7bdce1f80ff6568a05c4023065
alex <aleseibur@gmail.com>
2022-10-04 19:08:30 +0900

    AR fnish

---
+++ app/src/main/java/com/usa/tripjungle/VChatActivity.java
@@ -0,0 +1,12 @@
+package com.usa.tripjungle;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_chat);
+    }
+}
